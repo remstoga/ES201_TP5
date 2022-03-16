8d031b2c250b472d7690d94515dcd2ecc7761514
GASTAUD Remi <gastaud@salle.ensta.fr>
2022-03-16 14:02:33 +0100

    script done

--- script1.py
+++ script1.py
@@ -1,2 +1,19 @@
-def fonction():
-    pass
+from re import sub
+import subprocess
+
+results = open('data.log','w')
+
+m = 64
+# On ne prend que 16 threads Max, car avec 32 et plus il y a une erreur de segmentation
+nb_threads_l = [1,2,4,8,16]
+
+for nb_threads in nb_threads_l:
+    out = subprocess.run('$GEM5/build/ARM/gem5.fast $GEM5/configs/example/se.py --cpu-type=arm_detailed --caches -n ' + str(nb_threads) + ' -c test_omp -o "' + str(nb_threads) +' 64"',shell=True)
+    with open('m5out/stats.txt','r') as stats:
+        for num_cpu in range(nb_threads):
+            for ligne in stats:
+                if "number of cpu cycles simulated" in ligne:
+                    results.write("nombre de threads : " + str(nb_threads) + ' ' + str(ligne))
+        results.write('\n')
+
+results.close()
